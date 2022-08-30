c4229b708029979bddf8f818b840578959092e6a
yanina-fernandez <48021846+yanina-fernandez@users.noreply.github.com>
2022-08-30 16:45:06 -0300

    Add files via upload
    Tarea módulo 03

--- marvel/e_commerce/admin.py
+++ marvel/e_commerce/admin.py
@@ -1,34 +1,38 @@
 from django.contrib import admin
 
 # NOTE: Tenemos que importar los modelos con los que vamos a trabajar:
-# from e_commerce.models import *
+from e_commerce.models import *
 
 # Register your models here.
 
 # NOTE: Aquí personalizamos los campos en el Django Admin.
 
-# @admin.register(Comic)
-# class ComicsAdmin(admin.ModelAdmin):
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
     # NOTE: Para seleccionar los campos en la tabla de registros
-    # list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
 
     # NOTE: Filtro lateral de elementos:
-    # list_filter= ('marvel_id','title')
+    list_filter= ('marvel_id','title')
     
     # NOTE: Buscador de elementos en la columna:
-    # search_fields = ['title']
+    search_fields = ['title']
 
     # NOTE: Para seleccionar los campos en el registro. 
     # fields = ('marvel_id', 'title', 'stock_qty')
 
     # NOTE: Genera un campo desplegable con los registros seleccionados.
-    # fieldsets = (
-    #     (None, {
-    #         'fields': ('marvel_id', 'title', 'stock_qty')
-    #     }),
-    #     ('Advanced options', {
-    #         'classes': ('collapse',),
-    #         'fields': ('description','price', 'picture'),
-    #     }),
-    # )
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
     # pass
+    
+    @admin.register(wish_list)
+    class ComicsAdmin(admin.ModelAdmin):
+        pass
